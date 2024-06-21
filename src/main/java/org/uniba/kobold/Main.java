32d41142e56a1a12f8236ce620d23ac2ffa60a3c
IXRyu <fabiozippo03@hotmail.com>
2024-06-21 16:23:28 +0200

    ref(Main): Cambiato il nome del metodo e quindi la chiamata

--- src/main/java/org/uniba/kobold/Main.java
+++ src/main/java/org/uniba/kobold/Main.java
@@ -1,9 +1,9 @@
 package org.uniba.kobold;
 
-import org.uniba.kobold.gui.HubFrame;
+import org.uniba.kobold.gui.GuiHub;
 
 public class Main {
     public static void main(String[] args) {
-        new HubFrame();
+        new GuiHub();
     }
 }
