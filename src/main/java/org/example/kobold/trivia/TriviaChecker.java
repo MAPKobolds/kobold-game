f4272c2a95daad5e57ee7c15f880945b86492bda
f1r3k3rn <cronanacron@gmail.com>
2024-06-21 16:04:22 +0200

    feat(TriviaChecker): Creating TriviaChecker class for the parsing and creation of an object from the json

---
+++ src/main/java/org/example/kobold/trivia/TriviaChecker.java
@@ -0,0 +1,53 @@
+package org.example.kobold.trivia;
+
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TriviaChecker {
+    Quiz quiz;
+    int[] isCorrect;
+
+    public TriviaChecker() {
+        Gson gson = new Gson();
+        String response;
+
+        response = TriviaService.getTrivia();
+        quiz = gson.fromJson(response, Quiz.class);
+
+
+        isCorrect = new int[quiz.getResults().size()];
+        System.out.println(quiz);
+    }
+
+    public String[] getQuestions() {
+        String[] questions = new String[quiz.getResults().size()];
+        for (Result result : quiz.getResults()) {
+            questions[quiz.getResults().indexOf(result)] = result.getQuestion();
+        }
+        return questions;
+    }
+
+    public List<List<String>> getAnswersShuffled() {
+        List<List<String>> answers = new ArrayList<>();
+        for( Result result : quiz.getResults()) {
+            List<String> answersList = new ArrayList<>();
+            answersList.add(result.getCorrectAnswer());
+            answersList.addAll(result.getIncorrect_answers());
+            Collections.shuffle(answersList);
+            for(String answer : answersList) {
+                if(answer.equals(result.getCorrectAnswer())) {
+                    isCorrect[quiz.getResults().indexOf(result)] = answersList.indexOf(answer);
+                }
+            }
+            answers.add(answersList);
+        }
+
+        return answers;
+    }
+
+    public Boolean isCorrect(int questionIndex,int answerIndex) {
+        return isCorrect[questionIndex] == answerIndex;
+    }
+}
