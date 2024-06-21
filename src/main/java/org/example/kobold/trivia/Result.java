a009b83a2c2d2487c7efce05c48cae378f42281c
f1r3k3rn <cronanacron@gmail.com>
2024-06-21 16:03:06 +0200

    feat(Quiz): Creating Result class for the json parser

---
+++ src/main/java/org/example/kobold/trivia/Result.java
@@ -0,0 +1,49 @@
+package org.example.kobold.trivia;
+
+import java.util.List;
+
+class Result {
+    private String type;
+    private String difficulty;
+    private String category;
+    private String question;
+    private String correct_answer;
+    private List<String> incorrect_answers;
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getCorrectAnswer() {
+        return correct_answer;
+    }
+
+    public List<String> getIncorrect_answers() {
+        return incorrect_answers;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "type='" + type + '\'' +
+                ", difficulty='" + difficulty + '\'' +
+                ", category='" + category + '\'' +
+                ", question='" + question + '\'' +
+                ", correctAnswer='" + correct_answer + '\'' +
+                ", incorrectAnswers=" + incorrect_answers +
+                '}';
+    }
+}
+
