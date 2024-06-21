f0427380710b759b3d25c9e79d290393dbf0da69
IXRyu <fabiozippo03@hotmail.com>
2024-06-21 16:24:06 +0200

    ref(UtilMusic): Aggiunti metodi che gestiscono il bottone e la sua visualizzazione

--- src/main/java/org/uniba/kobold/util/UtilMusic.java
+++ src/main/java/org/uniba/kobold/util/UtilMusic.java
@@ -1,6 +1,7 @@
 package org.uniba.kobold.util;
 
 import javax.sound.sampled.*;
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 
@@ -38,15 +39,32 @@ public class UtilMusic extends Thread{
          * setMuted Method to mute
          * @param isMuted boolean to see if the music is muted or not
          */
-        public void setMuted(boolean isMuted) {
+        public void setMuted(boolean isMuted, JToggleButton muteMusicButton) {
                 this.isMuted = isMuted;
                 if (isMuted) {
                         stopClip();
+                        setOffText(muteMusicButton);
                 } else {
                         playClip();
+                        setOnText(muteMusicButton);
                 }
         }
 
+        /**
+         * setOnText Method to set the text on
+         * @param muteMusicButton JToggleButton to toggle music
+         */
+        public static void setOnText(JToggleButton muteMusicButton) {
+                muteMusicButton.setText("\uD83D\uDD0A");
+        }
+
+        /**
+         * setOffText Method to set the text off
+         * @param muteMusicButton JToggleButton to toggle music
+         */
+        public static void setOffText(JToggleButton muteMusicButton) {
+                muteMusicButton.setText("\uD83D\uDD07");
+        }
         /**
          * playClip Method to play the clip
          */
@@ -80,8 +98,12 @@ public class UtilMusic extends Thread{
                                 }
                         });
                         playClip();
-                } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-                        e.printStackTrace();
+                } catch (UnsupportedAudioFileException e) {
+                        System.err.println("Unsupported audio file: " + e.getMessage());
+                } catch (IOException e) {
+                        System.err.println("I/O error: " + e.getMessage());
+                } catch (LineUnavailableException e) {
+                        System.err.println("Line unavailable: " + e.getMessage());
                 }
         }
 }
