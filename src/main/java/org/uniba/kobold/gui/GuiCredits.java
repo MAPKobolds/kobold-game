e4a662b5441efcfbe02340576f443ca21927782f
IXRyu <fabiozippo03@hotmail.com>
2024-06-22 15:37:06 +0200

    ref(GuiCredits): Adattato alla modularità

--- src/main/java/org/uniba/kobold/gui/GuiCredits.java
+++ src/main/java/org/uniba/kobold/gui/GuiCredits.java
@@ -1,8 +1,8 @@
 package org.uniba.kobold.gui;
+
 import org.uniba.kobold.util.UtilMusic;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ItemEvent;
 
 /**
  * Class GuiCredits
@@ -12,124 +12,59 @@ public class GuiCredits extends JPanel {
     /**
      * Attributes of the class GuiCredits
      */
-    private GuiBackgroundPanel backgroundPanel = new GuiBackgroundPanel();
-    private JButton menuButton;
-    private JLabel creditsText;
-    private JPanel sgaramellaPanel;
-    private JPanel porcelliPanel;
-    private JPanel zippoPanel;
-    private JToggleButton muteMusicButton;
-    private int tokenSize = 150;
+    private static final GuiBackgroundPanel backgroundPanel = new GuiBackgroundPanel();
+    private static final String porcelliURL = "/img/PorcelliToken.png";
+    private static final String sgaramellaURL = "/img/SgaramellaToken.png";
+    private static final String zippoURL = "/img/ZippoToken.png";
+    private static final int tokenSize = 150;
+    private static JPanel sgaramellaPanel = new GuiGenericToken(sgaramellaURL, tokenSize).getToken();
+    private static JPanel porcelliPanel = new GuiGenericToken(porcelliURL, tokenSize).getToken();
+    private static JPanel zippoPanel = new GuiGenericToken(zippoURL, tokenSize).getToken();
+
+    private static JButton menuButton;
+    private static JLabel creditsText;
+    private static JToggleButton muteMusicButton;
+
 
     /**
      * Constructor of the class GuiCredits
      */
     public GuiCredits() {
+        initTokens();
         initComponents();
+    }
+
+    /**
+     * Method to initialize the tokens of the class GuiCredits
+     */
+    private void initTokens() {
+        GuiGenericToken.manageTokenLayout(zippoPanel, tokenSize);
+        GuiGenericToken.manageTokenLayout(sgaramellaPanel, tokenSize);
+        GuiGenericToken.manageTokenLayout(porcelliPanel, tokenSize);
     }
 
     /**
      * Method to initialize the components of the class GuiCredits
      */
     private void initComponents() {
-        menuButton = new JButton();
+        menuButton = new GuiGenericButton("Torna al Menu").getButton();
         creditsText = new JLabel();
         muteMusicButton = new JToggleButton();
 
         //Mute music button logic and clip management
-        muteMusicButton.setBackground(new java.awt.Color(204, 204, 204));
-        muteMusicButton.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
-        UtilMusic.setOnText(muteMusicButton);
-        muteMusicButton.addItemListener(e -> {
-            UtilMusic.getInstance().setMuted(e.getStateChange() == ItemEvent.SELECTED, muteMusicButton);
-        });
+        UtilMusic.initButton(muteMusicButton);
+        UtilMusic.manageButton(muteMusicButton);
+        muteMusicButton.setBounds(0, 0, 50, 50);
 
-        //Button Settings
-        menuButton.setBackground(new java.awt.Color(204, 204, 204));
-        menuButton.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        menuButton.setText("Torna al Menu");
-        menuButton.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
-        menuButton.addActionListener(e -> {
+        //menuButton logic
+        menuButton.addActionListener(_ -> {
             CardLayout menu = (CardLayout) getParent().getLayout();
             menu.show(getParent(), "Menu");
         });
 
-        //Layout Settings
-        zippoPanel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                setOpaque(false);
-                ImageIcon img = new ImageIcon("src/main/resources/img/ZippoToken.png");
-                Image image = img.getImage();
-                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
-            }
-        };
-        zippoPanel.setOpaque(false);
-        zippoPanel.setMinimumSize(new Dimension(tokenSize, tokenSize));
-        zippoPanel.setPreferredSize(new Dimension(tokenSize, tokenSize));
-
-        GroupLayout layoutZippo = new GroupLayout(zippoPanel);
-        zippoPanel.setLayout(layoutZippo);
-        layoutZippo.setHorizontalGroup(
-                layoutZippo.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGap(0, tokenSize, Short.MAX_VALUE)
-        );
-        layoutZippo.setVerticalGroup(
-                layoutZippo.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGap(0, tokenSize, Short.MAX_VALUE)
-        );
-
-        sgaramellaPanel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                ImageIcon img = new ImageIcon("src/main/resources/img/SgaramellaToken.png");
-                Image image = img.getImage();
-                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
-            }
-        };
-        sgaramellaPanel.setOpaque(false);
-        sgaramellaPanel.setMinimumSize(new Dimension(tokenSize, tokenSize));
-        sgaramellaPanel.setPreferredSize(new Dimension(tokenSize, tokenSize));
-
-        GroupLayout layoutSgaramella = new GroupLayout(sgaramellaPanel);
-        sgaramellaPanel.setLayout(layoutSgaramella);
-        layoutSgaramella.setHorizontalGroup(
-                layoutSgaramella.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGap(0, tokenSize, Short.MAX_VALUE)
-        );
-        layoutSgaramella.setVerticalGroup(
-                layoutSgaramella.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGap(0, tokenSize, Short.MAX_VALUE)
-        );
-
-        porcelliPanel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                ImageIcon img = new ImageIcon("src/main/resources/img/PorcelliToken.png");
-                Image image = img.getImage();
-                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
-            }
-        };
-        porcelliPanel.setOpaque(false);
-        porcelliPanel.setMinimumSize(new Dimension(tokenSize, tokenSize));
-        porcelliPanel.setPreferredSize(new Dimension(tokenSize, tokenSize));
-
-        GroupLayout layoutPorcelli = new GroupLayout(porcelliPanel);
-        porcelliPanel.setLayout(layoutPorcelli);
-        layoutPorcelli.setHorizontalGroup(
-                layoutPorcelli.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGap(0, tokenSize, Short.MAX_VALUE)
-        );
-        layoutPorcelli.setVerticalGroup(
-                layoutPorcelli.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGap(0, tokenSize, Short.MAX_VALUE)
-        );
-
         //Background layout settings
         creditsText.setText("Ci siamo impegnati molto porco dio");
+
         GroupLayout backgroundLayout = new GroupLayout(backgroundPanel);
         backgroundPanel.setLayout(backgroundLayout);
         backgroundLayout.setHorizontalGroup(
@@ -166,18 +101,9 @@ public class GuiCredits extends JPanel {
                                 .addComponent(menuButton, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
                                 .addGap(49, 49, 49))
         );
-        backgroundPanel.setLayout(backgroundLayout);
 
-        GroupLayout layout = new GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addComponent(backgroundPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addComponent(backgroundPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        //Background group layout manager
+        backgroundPanel.setLayout(backgroundLayout);
+        GuiBackgroundPanel.manageBackgroundLayout(this, backgroundPanel);
     }
 }
-
