b0476b532a05bbcd88772b89c5527d2581bc2397
frasga <s.francesco297@gmail.com>
2024-06-22 11:49:21 +0200

    feat(Repository): Create Repository abstract class

---
+++ src/main/java/org/example/kobold/rest/repositories/Repository.java
@@ -0,0 +1,27 @@
+package org.example.kobold.rest.repositories;
+
+import org.example.kobold.db.DBConnection;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public abstract class Repository<T> implements RepositoryInterface<T> {
+
+    public static String RELATION = "";
+    public Connection connection = DBConnection.getConnection();
+
+    protected Repository() {}
+
+    protected T getInsertedOne(Statement statement) throws SQLException {
+        ResultSet resultSet = statement.getGeneratedKeys();
+        int id = -1;
+
+        while (resultSet.next()) {
+            id = resultSet.getInt(1); // returns 0, 0
+        }
+
+        return this.getById(id);
+    }
+
+}
