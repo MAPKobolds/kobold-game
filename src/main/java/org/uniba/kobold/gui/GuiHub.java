6b10990cfa278c9e86b4b25867475132a2af905a
IXRyu <fabiozippo03@hotmail.com>
2024-06-22 11:44:19 +0200

    ref(GuiHub): Aggiunto il nuovo card layout

--- src/main/java/org/uniba/kobold/gui/GuiHub.java
+++ src/main/java/org/uniba/kobold/gui/GuiHub.java
@@ -10,14 +10,18 @@ import java.net.URL;
  */
 public class GuiHub extends JFrame {
 
+    /**
+     * Constructor of the class GuiHub
+     */
     public GuiHub() {
-        //JFRAME SETTINGS
+
+        //Jframe Settings
         setTitle("Cerignolus: Città dei Coboldi");
         setSize(800, 600);
         setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        //LOGO SETTINGS
+        //Logo Settings
         URL logo = GuiMenu.class.getResource("/img/BR.png");
         ImageIcon image = null;
         if (logo != null) {
@@ -25,18 +29,22 @@ public class GuiHub extends JFrame {
         }
         setIconImage(image != null ? image.getImage() : null);
 
-        // CARDS LAYOUT
+        //Card layout inits
         JPanel cards = new JPanel(new CardLayout());
         GuiMenu menu = new GuiMenu();
         GuiCredits credits = new GuiCredits();
         GuiLoadGame loadGame = new GuiLoadGame();
+        GuiLoadingScreen loadingScreen = new GuiLoadingScreen();
+        GuiGame game = new GuiGame();
 
-        // Add the panels to cards
-        cards.add(menu, "MenuPanel");
-        cards.add(credits, "CreditsPanel");
-        cards.add(loadGame, "SaveInstancesPanel");
+        //Card layout management
+        cards.add(menu, "Menu");
+        cards.add(credits, "Credits");
+        cards.add(loadGame, "SaveInstances");
+        cards.add(loadingScreen, "LoadingScreen");
+        cards.add(game, "Game");
 
-        // Start the frame
+        //Add cards to the frame and starts the music
         add(cards);
         pack();
         setLocationRelativeTo(null);
