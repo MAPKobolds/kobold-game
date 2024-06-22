01226d2a88c6244194ac1599c3e332515de9ce05
IXRyu <fabiozippo03@hotmail.com>
2024-06-22 15:36:45 +0200

    ref(GuiBackgroundPanel): Adesso generalizzo anche il layout

--- src/main/java/org/uniba/kobold/gui/GuiBackgroundPanel.java
+++ src/main/java/org/uniba/kobold/gui/GuiBackgroundPanel.java
@@ -2,6 +2,7 @@ package org.uniba.kobold.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 /**
  * Class GuiBackgroundPanel
@@ -11,13 +12,14 @@ public class GuiBackgroundPanel extends JPanel {
     /**
      * Attributes of the class GuiBackgroundPanel
      */
-    private int width = 800;
-    private int height = 600;
+    private final int width = 800;
+    private final int height = 600;
+    private final String backgroundPath = "/img/pporc.png";
 
     /**
      * Constructor of the class GuiBackgroundPanel
      */
-    public void GuiBackgroundPanel() {
+    public GuiBackgroundPanel() {
         initComponents();
     }
 
@@ -28,16 +30,32 @@ public class GuiBackgroundPanel extends JPanel {
         setMinimumSize(new Dimension(width, height));
         setPreferredSize(new Dimension(width, height));
         setRequestFocusEnabled(false);
-        paintComponent(getGraphics());
     }
-        /**
-     * Constructor of the class GuiBackgroundPanel
-     */
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        ImageIcon img = new ImageIcon("src/main/resources/img/pporc.png");
+        ImageIcon img = new ImageIcon(Objects.requireNonNull(getClass().getResource(backgroundPath)));
         Image image = img.getImage();
         g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+    }
+
+    /**
+     * Method to manage the layout of the background panel
+     *
+     * @param parent          the parent panel
+     * @param backgroundPanel the background panel
+     */
+    public static void manageBackgroundLayout(JPanel parent, JPanel backgroundPanel) {
+        GroupLayout layout = new GroupLayout(parent);
+        parent.setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addComponent(backgroundPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addComponent(backgroundPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
     }
 }
