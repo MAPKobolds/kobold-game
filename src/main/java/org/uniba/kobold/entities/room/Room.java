7a0f2bf714326d41b949a13c388ad208eb772436
frasga <s.francesco297@gmail.com>
2024-06-29 16:39:52 +0200

    ref(Room): Add bg image

--- src/main/java/org/uniba/kobold/entities/room/Room.java
+++ src/main/java/org/uniba/kobold/entities/room/Room.java
@@ -1,16 +1,19 @@
 package org.uniba.kobold.entities.room;
 
 import org.uniba.kobold.entities.item.Item;
+import javax.swing.*;
 import java.util.List;
 
 abstract public class Room {
     private final String name;
+    private final ImageIcon backgroundImage;
     private final List<Item> items;
     private final List<Character> characters;
     private final List<Command> commands;
 
-    public Room(String name, List<Item> items, List<Character> characters, List<Command> commands) {
+    public Room(String name, ImageIcon backgroundImage, List<Item> items, List<Character> characters, List<Command> commands) {
         this.name = name;
+        this.backgroundImage = backgroundImage;
         this.items = items;
         this.characters = characters;
         this.commands = commands;
@@ -26,6 +29,10 @@ abstract public class Room {
 
     public List<Character> getCharacters() {
         return characters;
+    }
+
+    public ImageIcon getBackgroundImage() {
+        return backgroundImage;
     }
 
     public final void interact(Command command) {
