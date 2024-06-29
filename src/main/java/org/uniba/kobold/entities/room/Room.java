f5fd5388cdad60b1bef72848a17c60d38089ab25
frasga <s.francesco297@gmail.com>
2024-06-29 15:58:43 +0200

    ref(Room): Remove commands dependencies

--- src/main/java/org/uniba/kobold/entities/room/Room.java
+++ src/main/java/org/uniba/kobold/entities/room/Room.java
@@ -2,46 +2,32 @@ package org.uniba.kobold.entities.room;
 
 import org.javatuples.Pair;
 import org.uniba.kobold.entities.item.Item;
-
 import java.util.List;
-import java.util.Map;
-
 
 abstract public class Room {
-        private final String name;
-        private final List<Item> items;
-        private final List<Character> characters;
-        private final Map<String, Pair<Boolean,List<String>>> interactions;
+    private final String name;
+    private final List<Item> items;
+    private final List<Character> characters;
+    private final List<Pair<String, String>> commands;
 
-        public Room(String name, List<Item> items, List<Character> characters, Map<String, Pair<Boolean,List<String>>> interactions) {
-            this.name = name;
-            this.items = items;
-            this.characters = characters;
-            this.interactions = interactions;
-        }
+    public Room(String name, List<Item> items, List<Character> characters, List<Pair<String, String>> commands) {
+        this.name = name;
+        this.items = items;
+        this.characters = characters;
+        this.commands = commands;
+    }
 
-        public String getName() {
-                return name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public List<Item> getItems() {
-                return items;
-        }
+    public List<Item> getItems() {
+        return items;
+    }
 
-        public List<Character> getCharacters() {
-                return characters;
-        }
+    public List<Character> getCharacters() {
+        return characters;
+    }
 
-        void isAccessible(String command) {
-                Pair<Boolean,List<String>> interact = interactions.get(command);
-                interact = interact.setAt0(true);
-        }
-
-        void isNotAccessible(String command) {
-                Pair<Boolean,List<String>> interact = interactions.get(command);
-                interact = interact.setAt0(false);
-        }
-
-        abstract void interact(String command);
-
+    abstract void interact(String command);
 }
