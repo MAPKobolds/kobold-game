30c7259fa76306303c87686d70b8c0ec6f46a62d
frasga <s.francesco297@gmail.com>
2024-06-30 18:33:09 +0200

    feat(Inventory): Make Inventory singleton

--- src/main/java/org/uniba/kobold/entities/inventory/Inventory.java
+++ src/main/java/org/uniba/kobold/entities/inventory/Inventory.java
@@ -4,7 +4,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Inventory {
+    private static Inventory instance;
     private static final Set<Item> items = new HashSet<>();
+
+    public static Inventory getInstance() {
+        if (instance == null) {
+            instance = new Inventory();
+        }
+        return instance;
+    }
 
     public static void addPiece(Item item) {
         if(!items.contains(item)) {
@@ -12,12 +20,16 @@ public class Inventory {
         }
     }
 
-    public void removePiece(Item item) {
+    public static void removePiece(Item item) {
         if(!items.contains(item)) {
             throw new Error("This item is not in the inventory");
         }
 
         items.remove(item);
+    }
+
+    public static boolean contains(String name) {
+        return items.stream().anyMatch(i -> ((Item) i).getName().equals(name));
     }
 
     public static Set<Item> getItems() {
