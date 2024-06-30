c089caccbf020ff32583c435f7f26aa8f84bb257
f1r3k3rn <cronanacron@gmail.com>
2024-06-30 10:55:15 +0200

    ref(Item): Adding Aliases

--- src/main/java/org/uniba/kobold/entities/inventory/Item.java
+++ src/main/java/org/uniba/kobold/entities/inventory/Item.java
@@ -2,16 +2,23 @@ package org.uniba.kobold.entities.inventory;
 
 import javax.swing.*;
 import java.util.Objects;
+import java.util.Set;
 
 public class Item {
     private String name;
+    private Set<String> alias;
     private String description;
     private ImageIcon image;
 
-    public Item(String name, String description, String imagePath) {
+    public Item(String name, Set<String> alias, String description, String imagePath) {
         this.name = name;
+        this.alias = alias;
         this.description = description;
         this.image = new ImageIcon(Objects.requireNonNull(getClass().getResource(imagePath)));
+    }
+
+    public Set<String> getAlias() {
+        return alias;
     }
 
     public String getDescription() {
