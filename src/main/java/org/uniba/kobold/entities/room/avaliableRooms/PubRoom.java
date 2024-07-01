a3e2beaf45f78cfb2c0d8cdf6b3a5a9f58041c29
frasga <s.francesco297@gmail.com>
2024-07-01 19:12:19 +0200

    feat(PubRoom): Create new Room

---
+++ src/main/java/org/uniba/kobold/entities/room/avaliableRooms/PubRoom.java
@@ -0,0 +1,51 @@
+package org.uniba.kobold.entities.room.avaliableRooms;
+
+import org.uniba.kobold.entities.room.Room;
+import org.uniba.kobold.entities.room.RoomInteractionResult;
+import org.uniba.kobold.entities.room.RoomInteractionResultType;
+import org.uniba.kobold.parser.ParserOutput;
+import javax.swing.*;
+import java.util.Arrays;
+
+public final class PubRoom extends Room {
+
+    public PubRoom() {
+        super("bar",
+                "Il bar è pieno di coboldi, di fronte a te c'è un bancone con un coboldo barista. " +
+                        "Sulla tua sinistra ci sono dei coboldi che giocano a carte" +
+                        "Sulla tua destra c'è un'uscita che da su uno SPIAZZALE",
+                new ImageIcon("/img/BR.png"),
+                Arrays.asList(),
+                Arrays.asList(),
+                Arrays.asList()
+        );
+    }
+
+    @Override
+    public RoomInteractionResult executeCommand(ParserOutput command) {
+        RoomInteractionResult result = new RoomInteractionResult(RoomInteractionResultType.NOTHING);
+
+        switch (command.getCommand().getName()) {
+            case "guarda davanti":
+                System.out.println("Guardi davanti e vedi il bancone del barista, è indaffarato a servire gli altri clienti ma avrà tempo per te");
+                break;
+            case "guarda dietro":
+                System.out.println("Guardi dietro e vedi il corridoio da dove sei arrivato");
+                break;
+            case "guarda destra":
+                System.out.println("Guardi a destra e un'uscita che da su uno SPIAZZALE");
+                break;
+            case "guarda sinistra":
+                System.out.println("Guardi a sinistra e vedi un gruppo di coboldi che giocano a blackjack, sei abbastanza ludopatico per giocare.");
+                break;
+            case "guarda sopra":
+                System.out.println("Una normale (Gauss shit) parete in legno");
+                break;
+            default:
+                System.out.println("Comando non valido");
+        }
+
+        return result;
+    }
+
+}
