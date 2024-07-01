7b4b6ac0734d3055f33447a15bef8bf32ec14042
frasga <s.francesco297@gmail.com>
2024-07-01 18:07:39 +0200

    ref(RoomInteractionResult): Add new constructor

--- src/main/java/org/uniba/kobold/entities/room/RoomInteractionResult.java
+++ src/main/java/org/uniba/kobold/entities/room/RoomInteractionResult.java
@@ -2,10 +2,14 @@ package org.uniba.kobold.entities.room;
 
 public class RoomInteractionResult {
     RoomInteractionResultType resultType;
-    String subject;
+    String subject = "";
 
     public RoomInteractionResult(RoomInteractionResultType resultType, String subject) {
         this.resultType = resultType;
         this.subject = subject;
+    }
+
+    public RoomInteractionResult(RoomInteractionResultType resultType) {
+        this.resultType = resultType;
     }
 }
