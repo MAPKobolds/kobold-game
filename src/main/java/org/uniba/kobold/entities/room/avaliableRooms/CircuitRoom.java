e6dd84e419144160a5be23829906b31687fe60f3
frasga <s.francesco297@gmail.com>
2024-07-02 15:41:33 +0200

    ref(CircuitRoom): Create CircuitRoom

---
+++ src/main/java/org/uniba/kobold/entities/room/avaliableRooms/CircuitRoom.java
@@ -0,0 +1,36 @@
+package org.uniba.kobold.entities.room.avaliableRooms;
+
+import org.uniba.kobold.entities.room.Room;
+import org.uniba.kobold.entities.room.RoomInteractionResult;
+import org.uniba.kobold.entities.room.RoomInteractionResultType;
+import org.uniba.kobold.parser.ParserOutput;
+import org.uniba.kobold.type.Command;
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.Set;
+
+public final class CircuitRoom extends Room {
+
+    public CircuitRoom() {
+        super("fucine",
+                "Nel circuito ci sono molti coboldi che scommettono, rivolgendosi alla CASSA." +
+                        "Alcuni hanno vinto alcune pezzi di auto e esultano, altri sono in completamente disperati per aver perso tutto.",
+                new ImageIcon("/img/BR.png"),
+                Arrays.asList(),
+                Arrays.asList(),
+                Arrays.asList(
+                    new Command("vai spiazzale", Set.of("muoviti"))
+                )
+        );
+    }
+
+    @Override
+    public RoomInteractionResult executeCommand(ParserOutput command) {
+        switch (command.getCommand().getName()) {
+            default:
+                System.out.println("Comando non valido");
+        }
+
+        return new RoomInteractionResult(RoomInteractionResultType.NOTHING);
+    }
+}
