b404c4f64b434ea7d1db4f3e3f6cb442b10a8dd1
frasga <s.francesco297@gmail.com>
2024-07-02 20:44:09 +0200

    feat(main): Start server in main

--- src/main/java/org/uniba/kobold/Main.java
+++ src/main/java/org/uniba/kobold/Main.java
@@ -3,11 +3,15 @@ package org.uniba.kobold;
 import org.uniba.kobold.game.Game;
 import org.uniba.kobold.gui.GuiHub;
 import org.uniba.kobold.rest.Server;
+import org.uniba.kobold.socket.WebServerSocket;
+
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void c(String[] args) throws IOException {
         Server.startServer();
+        WebServerSocket.startServer();
+
         Game g = new Game();
 
         g.executeCommand("prendi mantello");
