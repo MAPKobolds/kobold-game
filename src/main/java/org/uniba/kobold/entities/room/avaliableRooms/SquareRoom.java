7ba3e4619aa05d3c20837d15c3d95fa90e91b7c8
frasga <s.francesco297@gmail.com>
2024-07-02 15:28:19 +0200

    ref(SquareRoom): Create SquareRoom

---
+++ src/main/java/org/uniba/kobold/entities/room/avaliableRooms/SquareRoom.java
@@ -0,0 +1,45 @@
+package org.uniba.kobold.entities.room.avaliableRooms;
+
+import org.uniba.kobold.entities.inventory.availableItems.Cloak;
+import org.uniba.kobold.entities.room.Room;
+import org.uniba.kobold.entities.room.RoomInteractionResult;
+import org.uniba.kobold.entities.room.RoomInteractionResultType;
+import org.uniba.kobold.parser.ParserOutput;
+import org.uniba.kobold.type.Command;
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.Set;
+
+public final class SquareRoom extends Room {
+
+    public SquareRoom() {
+        super("spiazzale",
+                "Lo spiazzale si trova all'uscita del BAR ed è un posto di ritrovo dei coboldi, circondato da altri palazzi abitati da altri coboldi." +
+                        "Di fronte a te noti dei cartelli che indicano dei luoghi di interesse: " +
+                        "\nFUCINE" +
+                        "\nPISTA" +
+                        "\nPALAZZO" +
+                        "\nGENERATORE",
+                new ImageIcon("/img/BR.png"),
+                Arrays.asList(new Cloak()),
+                Arrays.asList(),
+                Arrays.asList(
+                    new Command("vai bar", Set.of("muoviti")),
+                    new Command("vai fucine", Set.of("muoviti")),
+                    new Command("vai pista", Set.of("muoviti")),
+                    new Command("vai pista ", Set.of("muoviti")),
+                    new Command("vai generatore", Set.of("muoviti"))
+                )
+        );
+    }
+
+    @Override
+    public RoomInteractionResult executeCommand(ParserOutput command) {
+        switch (command.getCommand().getName()) {
+            default:
+                System.out.println("Comando non valido");
+        }
+
+        return new RoomInteractionResult(RoomInteractionResultType.NOTHING);
+    }
+}
