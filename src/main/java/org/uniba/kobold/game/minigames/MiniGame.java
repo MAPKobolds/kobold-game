ee2cba04091288b98609744bc20b0ff354f42e29
frasga <s.francesco297@gmail.com>
2024-07-03 17:54:56 +0200

    ref(MiniGame): Refactor code spaces

--- src/main/java/org/uniba/kobold/game/minigames/MiniGame.java
+++ src/main/java/org/uniba/kobold/game/minigames/MiniGame.java
@@ -8,25 +8,24 @@ import org.uniba.kobold.type.Command;
 import java.util.ArrayList;
 
 public abstract class MiniGame {
+    String description;
+    ArrayList<Command> commands = new ArrayList<>();
 
-        String description;
-        ArrayList<Command> commands = new ArrayList<>();
+    public abstract MiniGameInteraction play(ParserOutput output);
 
-        public abstract MiniGameInteraction play(ParserOutput output);
+    public void addItem(Item item) {
+        Inventory.addPiece(item);
+    }
 
-        public void addItem(Item item) {
+    public ArrayList<Command> getCommands() {
+        return commands;
+    }
+
+    public void removeItem(Item item) {
             Inventory.addPiece(item);
-        }
+    }
 
-        public ArrayList<Command> getCommands() {
-            return commands;
-        }
-
-        public void removeItem(Item item) {
-                Inventory.addPiece(item);
-        }
-
-        public String getDescription() {
-            return description;
-        }
+    public String getDescription() {
+        return description;
+    }
 }
