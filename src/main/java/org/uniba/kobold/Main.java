73f436763c514e6863d86d905fda0f281cb90a0e
f1r3k3rn <cronanacron@gmail.com>
2024-07-03 17:13:37 +0200

    ref(Main): Now we can test

--- src/main/java/org/uniba/kobold/Main.java
+++ src/main/java/org/uniba/kobold/Main.java
@@ -1,20 +1,17 @@
 package org.uniba.kobold;
 
-import org.uniba.kobold.game.Game;
+
 import org.uniba.kobold.gui.GuiHub;
 import org.uniba.kobold.rest.Server;
+import org.uniba.kobold.tester.Tester;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Server.startServer();
-        Game g = new Game();
 
-        g.executeCommand("prendi mantello");
-        g.executeCommand("vai corridoio");
-        g.executeCommand("guarda a destra");
-        g.executeCommand("parla guardie");
-        g.executeCommand("vai bar");
+        Tester.main(args);
 
         new GuiHub();
 
