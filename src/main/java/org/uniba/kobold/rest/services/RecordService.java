0c912fd58028ca53baac2c4b64be82f7bb4a39ae
frasga <s.francesco297@gmail.com>
2024-07-04 18:15:10 +0200

    feat(RecordService): Create service to get best n element

--- src/main/java/org/uniba/kobold/rest/services/RecordService.java
+++ src/main/java/org/uniba/kobold/rest/services/RecordService.java
@@ -4,6 +4,8 @@ import org.uniba.kobold.rest.CRUDInterface;
 import org.uniba.kobold.rest.models.Record;
 import org.uniba.kobold.rest.repositories.RecordRepository;
 
+import java.util.List;
+
 public class RecordService implements CRUDInterface<Record> {
 
     RecordRepository recordRepository = new RecordRepository();
@@ -26,5 +28,13 @@ public class RecordService implements CRUDInterface<Record> {
     @Override
     public Record getById(int id) {
         return recordRepository.getById(id);
+    }
+
+    public List<Record> getBest(int limit) {
+        if(limit <= 0) {
+            throw new  Error("Invalid limit");
+        }
+
+        return recordRepository.getBestTime(limit);
     }
 }
