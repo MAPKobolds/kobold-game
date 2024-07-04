3608c81ee0afbb3eb1156e3a8c572fcc7c13870d
frasga <s.francesco297@gmail.com>
2024-07-04 17:41:20 +0200

    ref(Record): Ref constructor

--- src/main/java/org/uniba/kobold/rest/models/Record.java
+++ src/main/java/org/uniba/kobold/rest/models/Record.java
@@ -6,13 +6,13 @@ public class Record extends Base {
     private String name;
     private long time;
 
-    public Record(int id, String name, Time time) {
+    public Record(String name, Time time, int id) {
         this.id = id;
         this.name = name;
         this.time = time.getTime();
     }
 
-    public Record(int id, String name, long time) {
+    public Record(String name, long time, int id) {
         this.id = id;
         this.name = name;
         this.time = time;
