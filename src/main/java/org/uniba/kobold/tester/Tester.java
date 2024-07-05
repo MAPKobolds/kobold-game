13c597573a6dfad0bd7d107563daadc6f7d6a87f
f1r3k3rn <cronanacron@gmail.com>
2024-07-05 18:26:31 +0200

    ref(Tester): Playable all rooms execpts for 2

--- src/main/java/org/uniba/kobold/tester/Tester.java
+++ src/main/java/org/uniba/kobold/tester/Tester.java
@@ -1,9 +1,6 @@
 package org.uniba.kobold.tester;
 
 import org.uniba.kobold.entities.inventory.Inventory;
-import org.uniba.kobold.entities.inventory.availableItems.Bill;
-import org.uniba.kobold.entities.inventory.availableItems.FireMaul;
-import org.uniba.kobold.entities.inventory.availableItems.GinMoncello;
 import org.uniba.kobold.game.Game;
 
 import java.io.IOException;
@@ -14,19 +11,20 @@ public class Tester {
 
         Game game = new Game();
 
-        Inventory.addMoney(1000);
-        Inventory.addPiece(new GinMoncello());
-        Inventory.addPiece(new FireMaul());
-
+        Inventory.addMoney(800);
         game.executeCommand("prendi mantello");
         game.executeCommand("vai corridoio");
         game.executeCommand("parla guardie");
-        game.executeCommand("vai bar");
-        game.executeCommand("gioca");
+        game.executeCommand("vai taverna");
+        game.executeCommand("vai spiazzale");
+        game.executeCommand("vai fucine");
 
         while (true) {
             String input = System.console().readLine();
             game.executeCommand(input);
+            if (input.equals("sussylabaka")) {
+                break;
+            }
         }
     }
 }
