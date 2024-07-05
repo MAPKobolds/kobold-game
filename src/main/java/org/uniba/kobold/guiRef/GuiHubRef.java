7a352fc9ec3fdf2ba0b758183f6c9740d5aaaa1e
IXRyu <fabiozippo03@hotmail.com>
2024-07-05 18:03:21 +0200

    ref(guiref): frame completato

--- src/main/java/org/uniba/kobold/guiRef/GuiHubRef.java
+++ src/main/java/org/uniba/kobold/guiRef/GuiHubRef.java
@@ -1,87 +1,86 @@
 package org.uniba.kobold.guiRef;
 
 import org.uniba.kobold.util.UtilMusic;
-
-/**
- * Main hub of the game
- */
-/*public class GuiHubRef extends JFrame {
-
-    /**
-     * Attributes of the GuiHub class
-     */
-    /*private final static int width = 1000;
-    private final static int height = 700;
-    public static JPanel masterPanel = new JPanel(new CardLayout());*/
-
-    /**
-     * Constructor of the class GuiHub
-     */
-    /*public GuiHubRef() {
-        //JFrame Settings
-        setTitle("Cerignolus: Città dei Coboldi");
-        setSize(width, height);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setResizable(true);
-
-        //Logo Settings
-        URL logo = GuiHubRef.class.getResource("/img/BR.png");
-        ImageIcon image = null;
-        if (logo != null) {
-            image = new ImageIcon(logo);
-        }
-        setIconImage(image != null ? image.getImage() : null);
-
-        //Add cards to the frame and starts the music
-        masterPanel = addMenu();
-        add(masterPanel);
-        setLocationRelativeTo(null);
-        setVisible(true);
-        UtilMusic music = UtilMusic.getInstance();
-        //music.start();
-    }
-
-    private JPanel addMenu() {
-        JPanel cards = new JPanel(new CardLayout());
-
-        GuiMenuRef menuRef = new GuiMenuRef();
-        cards.add(menuRef, "menu");
-        return cards;
-    }
-}*/
-
 import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
 
 public class GuiHubRef extends JFrame {
 
     private JPanel masterPanel;
     private JToggleButton muteMusicButton;
     private JToolBar toolBar;
+    private final int WIDTH = 1000;
+    private final int HEIGHT = 700;
+
 
     public GuiHubRef() {
         initComponents();
     }
-    
+
     private void initComponents() {
         toolBar = new JToolBar();
         muteMusicButton = new JToggleButton();
         masterPanel = new JPanel();
 
+        //JFrame Settings
+        setTitle("Cerignolus: Città dei Coboldi");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(1000, 700));
+        setResizable(true);
+        setLogo();
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setVisible(true);
+
+        masterPanel = getInitialPanel();
+        add(masterPanel);
+
+        UtilMusic music = UtilMusic.getInstance();
+        //music.start();
 
         toolBar.setRollover(true);
         UtilMusic.initButton(muteMusicButton);
         toolBar.add(muteMusicButton);
 
-        GroupLayout jPanel1Layout = new GroupLayout(masterPanel);
-        masterPanel.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-                jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+        setLayout();
+        pack();
+    }
+
+    private void setLogo() {
+        URL logo = GuiHubRef.class.getResource("/img/BR.png");
+        ImageIcon image = null;
+        if (logo != null) {
+            image = new ImageIcon(logo);
+        }
+        setIconImage(image != null ? image.getImage() : null);
+    }
+
+    private JPanel getInitialPanel() {
+        JPanel cards = new JPanel(new CardLayout());
+
+        GuiMenuRef menuRef = new GuiMenuRef();
+        cards.add(menuRef, "menu");
+        return cards;
+    }
+
+
+    private void switchPanel(JPanel panel, String name) {
+        if(panel.getComponents().length < 2) {
+            throw new Error("There must be at least 2 panels");
+        }
+
+        masterPanel.remove(masterPanel.getComponents().length);
+        masterPanel.add(panel, name);
+    }
+
+    private void setLayout() {
+        GroupLayout masterPanelLayout = new GroupLayout(masterPanel);
+        masterPanel.setLayout(masterPanelLayout);
+        masterPanelLayout.setHorizontalGroup(
+                masterPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGap(0, 0, Short.MAX_VALUE)
         );
-        jPanel1Layout.setVerticalGroup(
-                jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+        masterPanelLayout.setVerticalGroup(
+                masterPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGap(0, 663, Short.MAX_VALUE)
         );
 
@@ -100,15 +99,5 @@ public class GuiHubRef extends JFrame {
                                 .addComponent(masterPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                                 .addContainerGap())
         );
-
-        pack();
     }
-
-    /**
-     * @param args the command line arguments
-     */
-    /*public static void main(String args[]) {
-
-
-    }*/
 }
