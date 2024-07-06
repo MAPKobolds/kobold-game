7875cfbb3ac2875639e4752d21c3cf76b9df0d31
f1r3k3rn <cronanacron@gmail.com>
2024-07-06 20:43:36 +0200

    feat(SteeringWheel): Adding SteeringWheel

---
+++ src/main/java/org/uniba/kobold/entities/inventory/availableItems/SteeringWheel.java
@@ -0,0 +1,16 @@
+package org.uniba.kobold.entities.inventory.availableItems;
+
+import org.uniba.kobold.entities.inventory.Item;
+
+import java.util.Set;
+
+public class SteeringWheel extends Item {
+    public SteeringWheel() {
+        super(
+                "volante",
+                Set.of("volante","manubrio","manubrio d'oro"),
+                "Il volante della tua auto, è plastificato e lucido",
+                "/img/BR.png"
+        );
+    }
+}
